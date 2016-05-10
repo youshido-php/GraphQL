3d874f152c6d934c11377cdf255aac8ed1e46825
vasilportey <portey@gmail.com>
2016-05-10 18:44:28 +0300

    add connection definition

--- src/Relay/Connection.php
+++ src/Relay/Connection.php
@@ -9,6 +9,10 @@ namespace Youshido\GraphQL\Relay;
 
 
 use Youshido\GraphQL\Type\Config\TypeConfigInterface;
+use Youshido\GraphQL\Type\ListType\ListType;
+use Youshido\GraphQL\Type\NonNullType;
+use Youshido\GraphQL\Type\Object\AbstractObjectType;
+use Youshido\GraphQL\Type\Object\ObjectType;
 use Youshido\GraphQL\Type\TypeMap;
 
 class Connection
@@ -41,6 +45,79 @@ class Connection
         $config
             ->addArgument('before', TypeMap::TYPE_STRING)
             ->addArgument('last', TypeMap::TYPE_INT);
+    }
+
+    /**
+     * @param AbstractObjectType $type
+     * @param null|string        $name
+     * @param array              $edgeFields
+     * @param array              $connectionFields
+     *
+     * @return ObjectType
+     */
+    public function connectionDefinition($type, $name = null, $edgeFields = [], $connectionFields = [])
+    {
+        $name = $name ?: $type->getName();
+
+        $edgeType = new ObjectType([
+            'name'        => $name . 'Edge',
+            'description' => 'An edge in a connection.',
+            'fields'      => array_merge([
+                'node'   => [
+                    'type'        => $type,
+                    'description' => 'The item at the end of the edge'
+                ],
+                'cursor' => [
+                    'type'        => TypeMap::TYPE_STRING,
+                    'description' => 'A cursor for use in pagination'
+                ]
+            ], $edgeFields)
+        ]);
+
+        $connectionType = new ObjectType([
+            'name'        => $name . 'Connection',
+            'description' => 'A connection to a list of items.',
+            'fields'      => array_merge([
+                'pageInfo' => [
+                    'type'        => new NonNullType(self::getPageInfoType()),
+                    'description' => 'Information to aid in pagination.'
+                ],
+                'edges'    => [
+                    'type'        => new ListType($edgeType),
+                    'description' => 'A list of edges.'
+                ]
+            ], $connectionFields)
+        ]);
+
+        return $connectionType;
+    }
+
+    public static function getPageInfoType()
+    {
+        //todo: maybe use object singleton
+
+        return new ObjectType([
+            'name'        => 'PageInfo',
+            'description' => 'Information about pagination in a connection.',
+            'fields'      => [
+                'hasNextPage'     => [
+                    'type'        => new NonNullType(TypeMap::TYPE_BOOLEAN),
+                    'description' => 'When paginating forwards, are there more items?'
+                ],
+                'hasPreviousPage' => [
+                    'type'        => new NonNullType(TypeMap::TYPE_BOOLEAN),
+                    'description' => 'When paginating backwards, are there more items?'
+                ],
+                'startCursor'     => [
+                    'type'        => TypeMap::TYPE_STRING,
+                    'description' => 'When paginating backwards, the cursor to continue.'
+                ],
+                'endCursor'       => [
+                    'type'        => TypeMap::TYPE_STRING,
+                    'description' => 'When paginating forwards, the cursor to continue.'
+                ],
+            ]
+        ]);
     }
 
 }
