ea66d00c8c55a0b36c865392843129a6a6e8be21
Alexandr Viniychuk <a@viniychuk.com>
2016-07-12 23:11:02 -0400

    fixed issue https://github.com/Youshido/GraphQL/issues/10

--- src/Execution/Processor.php
+++ src/Execution/Processor.php
@@ -258,7 +258,7 @@ class Processor
         }
 
         if ($resolveFunction = $field->getConfig()->getResolveFunction()) {
-            $resolveInfo   = new ResolveInfo($field, [$fieldAst], $field->getType(), $this->executionContext);
+            $resolveInfo = new ResolveInfo($field, [$fieldAst], $field->getType(), $this->executionContext);
 
             $resolverValue = $resolveFunction($resolved ? $resolverValue : $contextValue, $fieldAst->getKeyValueArguments(), $resolveInfo);
         }
@@ -322,18 +322,22 @@ class Processor
 
                 if ($fieldAst->getName() == self::TYPE_NAME_QUERY) {
                     $fieldResolvedValue = [$alias => $queryType->getName()];
-                } elseif ($fieldAst instanceof Query) {
-                    $fieldValue         = $this->processQueryAST($fieldAst, $currentType->getField($fieldAst->getName()), $resolvedValue);
-                    $fieldResolvedValue = [$alias => $fieldValue];
-                } elseif ($fieldAst instanceof FieldAst) {
-
+                } else {
                     if (!$this->resolveValidator->objectHasField($currentType, $fieldAst)) {
                         $fieldResolvedValue = null;
                     } else {
-                        $fieldResolvedValue = [
-                            $alias => $this->processFieldAST($fieldAst, $currentType->getField($fieldAst->getName()), $resolvedValue)
-                        ];
+                        if ($fieldAst instanceof Query) {
+                            $queryAst           = $currentType->getField($fieldAst->getName());
+                            $fieldValue         = $queryAst ? $this->processQueryAST($fieldAst, $queryAst, $resolvedValue) : null;
+                            $fieldResolvedValue = [$alias => $fieldValue];
+                        } elseif ($fieldAst instanceof FieldAst) {
+                            $fieldResolvedValue = [
+                                $alias => $this->processFieldAST($fieldAst, $currentType->getField($fieldAst->getName()), $resolvedValue)
+                            ];
+                        }
                     }
+
+
                 }
             }
 
