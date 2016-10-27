c8301d2cdf917be62f4002dd5adc9d16921977d8
vasilportey <portey@gmail.com>
2016-10-27 13:27:04 +0300

    formating

--- src/Execution/Visitor/AbstractQueryVisitor.php
+++ src/Execution/Visitor/AbstractQueryVisitor.php
@@ -27,40 +27,43 @@ namespace Youshido\GraphQL\Execution\Visitor;
 
 use Youshido\GraphQL\Config\Field\FieldConfig;
 
-abstract class AbstractQueryVisitor {
+abstract class AbstractQueryVisitor
+{
 
-  /**
-   * @var int initial value of $this->memo
-   */
-  protected $initialValue = 0;
+    /**
+     * @var int initial value of $this->memo
+     */
+    protected $initialValue = 0;
 
-  /**
-   * @var mixed the accumulator
-   */
-  protected $memo;
+    /**
+     * @var mixed the accumulator
+     */
+    protected $memo;
 
-  /**
-   * AbstractQueryVisitor constructor.
-   */
-  public function __construct() {
-    $this->memo = $this->initialValue;
-  }
+    /**
+     * AbstractQueryVisitor constructor.
+     */
+    public function __construct()
+    {
+        $this->memo = $this->initialValue;
+    }
 
-  /**
-   * @return mixed getter for the memo, in case callers want to inspect it after a process run
-   */
-  public function getMemo() {
-    return $this->memo;
-  }
+    /**
+     * @return mixed getter for the memo, in case callers want to inspect it after a process run
+     */
+    public function getMemo()
+    {
+        return $this->memo;
+    }
 
-  /**
-   * Visit a query node.  See class docstring.
-   *
-   * @param array       $args
-   * @param FieldConfig $fieldConfig
-   * @param int         $childScore
-   *
-   * @return int|null
-   */
-  abstract public function visit(array $args, FieldConfig $fieldConfig, $childScore = 0);
+    /**
+     * Visit a query node.  See class docstring.
+     *
+     * @param array       $args
+     * @param FieldConfig $fieldConfig
+     * @param int         $childScore
+     *
+     * @return int|null
+     */
+    abstract public function visit(array $args, FieldConfig $fieldConfig, $childScore = 0);
 }
--- src/Execution/Visitor/MaxComplexityQueryVisitor.php
+++ src/Execution/Visitor/MaxComplexityQueryVisitor.php
@@ -14,42 +14,48 @@ namespace Youshido\GraphQL\Execution\Visitor;
 
 use Youshido\GraphQL\Config\Field\FieldConfig;
 
-class MaxComplexityQueryVisitor extends AbstractQueryVisitor {
+class MaxComplexityQueryVisitor extends AbstractQueryVisitor
+{
 
-  /**
-   * @var int max score allowed before throwing an exception (causing processing to stop)
-   */
-  public $maxScore;
+    /**
+     * @var int max score allowed before throwing an exception (causing processing to stop)
+     */
+    public $maxScore;
 
-  /**
-   * @var int default score for nodes without explicit cost functions
-   */
-  protected $defaultScore = 1;
+    /**
+     * @var int default score for nodes without explicit cost functions
+     */
+    protected $defaultScore = 1;
 
-  /**
-   * MaxComplexityQueryVisitor constructor.
-   *
-   * @param int $max max allowed complexity score
-   */
-  public function __construct($max) {
-    parent::__construct();
+    /**
+     * MaxComplexityQueryVisitor constructor.
+     *
+     * @param int $max max allowed complexity score
+     */
+    public function __construct($max)
+    {
+        parent::__construct();
 
-    $this->maxScore = $max;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function visit(array $args, FieldConfig $fieldConfig, $childScore = 0) {
-    $cost = $fieldConfig->get('cost');
-    if (is_callable($cost)) {
-      $cost = $cost($args, $fieldConfig, $childScore);
+        $this->maxScore = $max;
     }
-    $cost = $cost ?: $this->defaultScore;
-    $this->memo += $cost;
-    if ($this->memo > $this->maxScore) {
-      throw new \Exception('query exceeded max allowed complexity of ' . $this->maxScore);
+
+    /**
+     * {@inheritdoc}
+     */
+    public function visit(array $args, FieldConfig $fieldConfig, $childScore = 0)
+    {
+        $cost = $fieldConfig->get('cost');
+        if (is_callable($cost)) {
+            $cost = $cost($args, $fieldConfig, $childScore);
+        }
+
+        $cost = $cost ?: $this->defaultScore;
+        $this->memo += $cost;
+
+        if ($this->memo > $this->maxScore) {
+            throw new \Exception('query exceeded max allowed complexity of ' . $this->maxScore);
+        }
+
+        return $cost;
     }
-    return $cost;
-  }
 }
