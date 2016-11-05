b61aeb9ece5151daac2c088a17e9003fbf6a8ce8
Dan Bruder <dbruder@rapiddg.com>
2016-11-05 09:09:55 -0400

    Test for #74: Support fragments on Unions and Interfaces

--- Tests/Library/Validator/ResolveValidatorTest.php
+++ Tests/Library/Validator/ResolveValidatorTest.php
@@ -30,6 +30,7 @@ use Youshido\GraphQL\Validator\ResolveValidator\ResolveValidator;
 use Youshido\Tests\DataProvider\TestEnumType;
 use Youshido\Tests\DataProvider\TestInterfaceType;
 use Youshido\Tests\DataProvider\TestObjectType;
+use Youshido\Tests\DataProvider\TestUnionType;
 use Youshido\Tests\DataProvider\TestSchema;
 
 class ResolveValidatorTest extends \PHPUnit_Framework_TestCase
@@ -78,6 +79,24 @@ class ResolveValidatorTest extends \PHPUnit_Framework_TestCase
       $fragment          = new Fragment('name', 'User', []);
       $fragmentReference = new FragmentReference('name');
       $validator->assertValidFragmentForField($fragment, $fragmentReference, new NonNullType($userType));
+    }
+
+    public function testValidFragmentTypeWithUnion()
+    {
+      $validator = new ResolveValidator(new ExecutionContext(new TestSchema()));
+
+      $unionType = new ObjectType([
+        'name' => 'Mammals',
+        'type'    => new TestUnionType(),
+        'resolve' => function ($value, $args, ResolveInfo $info) {
+          $testObject = new TestObjectType();
+          return [$testObject->getData()];
+        }
+      ]);
+
+      $fragment          = new Fragment('name', 'TestObject', ['name']);
+      $fragmentReference = new FragmentReference('name');
+      $validator->assertValidFragmentForField($fragment, $fragmentReference, $unionType);
     }
 
     /**
