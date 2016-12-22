d9f67422bb0dfacdf4e10eca6cb2f5ae0165e3d7
Fritz Gerneth <gerneth.fritz@funct.com>
2016-12-22 11:38:22 +0100

    Fix typo in exception

--- src/Execution/Processor.php
+++ src/Execution/Processor.php
@@ -471,7 +471,7 @@ class Processor
         $resolvedType = $type->resolveType($resolvedValue);
 
         if (!$resolvedType) {
-            throw new ResolveException('Resoling function must return type');
+            throw new ResolveException('Resolving function must return type');
         }
 
         if ($type instanceof AbstractInterfaceType) {
