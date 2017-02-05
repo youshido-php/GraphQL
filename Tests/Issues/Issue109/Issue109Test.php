718f9d31830454d75628ed2403259bded3ec1330
Alexandr Viniychuk <a@viniychuk.com>
2017-02-05 10:25:07 -0500

    Issue109 – variable access on the higher level

---
+++ Tests/Issues/Issue109/Issue109Test.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Youshido\Tests\Issues\Issue109;
+
+use Youshido\GraphQL\Execution\Processor;
+
+class Issue109Test extends \PHPUnit_Framework_TestCase
+{
+
+    public function testInternalVariableArgument()
+    {
+        $schema    = new Issue109Schema();
+        $processor = new Processor($schema);
+        $response  = $processor->processPayload('
+query ($postId: Int, $commentId: Int) { 
+    latestPost(id: $postId) { 
+        id(comment_id: $commentId), comments(comment_id: $commentId) {
+            comment_id
+        } 
+    } 
+}',
+            [
+                'postId'    => 1,
+                'commentId' => 100
+            ])->getResponseData();
+        $this->assertEquals(['data' => ['latestPost' => [
+            'id'       => 1,
+            'comments' => [
+                ['comment_id' => 100]
+            ]
+        ]]], $response);
+    }
+}
