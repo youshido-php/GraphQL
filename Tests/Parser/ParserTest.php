6490142891ab87281f0276b9f0f2bbcd64048e1e
Matthew Litwin <matthew.litwin@mheducation.com>
2017-12-22 13:26:30 -0800

    Php 5 support? Nice to see the travisCI tests BTW trying this - I don`t have php5 on my system ...

--- Tests/Parser/ParserTest.php
+++ Tests/Parser/ParserTest.php
@@ -131,8 +131,8 @@ GRAPHQL;
                 new Token(Token::TYPE_STRING, 1, 3, "\f"),
                 new Token(Token::TYPE_STRING, 1, 3, "\n"),
                 new Token(Token::TYPE_STRING, 1, 3, "\r"),     
-                new Token(Token::TYPE_STRING, 1, 3, "\u{0008}"),         
-                new Token(Token::TYPE_STRING, 1, 7, "\u{ABCD}")            
+                new Token(Token::TYPE_STRING, 1, 3, chr(8)),         
+                new Token(Token::TYPE_STRING, 1, 7, html_entity_decode("&#xABCD;", ENT_QUOTES, 'UTF-8'))            
             ]
         );
     }
