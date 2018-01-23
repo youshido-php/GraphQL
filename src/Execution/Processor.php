youshido-php/GraphQL

commit 8a01f948183e5d93f030e50e4016713e13cdb0e2

issue 151 fix

src/Execution/Processor.php
@@ -342,7 +342,7 @@ class Processor
                             }
                         }
 
-                        continue;
+                        continue 2;
                     }
 
                     $result = array_replace_recursive($result, $this->collectResult($field, $type, $astField, $resolvedValue));
@@ -354,17 +354,16 @@ class Processor
                     $astFragmentModel = $astFragment->getModel();
                     $typeName         = $type->getName();
 
-                    if ($typeName !== $astFragmentModel) {
+                    if ($typeName !== $astFragmentModel && $type->getInterfaces()) {
                         foreach ($type->getInterfaces() as $interface) {
                             if ($interface->getName() === $astFragmentModel) {
                                 $result = array_replace_recursive($result, $this->collectResult($field, $type, $astFragment, $resolvedValue));
 
                                 break;
                             }
-
                         }
 
-                        continue;
+                        continue 2;
                     }
 
                     $result = array_replace_recursive($result, $this->collectResult($field, $type, $astFragment, $resolvedValue));

Tests/Issues/Issue151/Issue151Test.php
@@ -1,0 +1,89 @@
+<?php
+
+namespace Youshido\Tests\Issues\Issue116Test;
+
+use Youshido\GraphQL\Execution\Processor;
+use Youshido\GraphQL\Schema\Schema;
+use Youshido\GraphQL\Type\ListType\ListType;
+use Youshido\GraphQL\Type\Object\ObjectType;
+use Youshido\GraphQL\Type\Scalar\IdType;
+use Youshido\GraphQL\Type\Scalar\StringType;
+use Youshido\GraphQL\Type\Union\UnionType;
+
+class Issue151Test extends \PHPUnit_Framework_TestCase
+{
+    public function testInternalVariableArgument()
+    {
+        $type1 = new ObjectType([
+            'name'   => 'Type1',
+            'fields' => [
+                'id'   => new IdType(),
+                'name' => new StringType(),
+            ],
+        ]);
+        $type2 = new ObjectType([
+            'name'   => 'Type2',
+            'fields' => [
+                'id'    => new IdType(),
+                'title' => new StringType(),
+            ],
+        ]);
+
+        $unionType = new UnionType([
+            'name'        => 'Union',
+            'types'       => [$type1, $type2],
+            'resolveType' => function ($value) use ($type1, $type2) {
+                if (isset($value['name'])) {
+                    return $type1;
+                }
+
+                return $type2;
+            },
+        ]);
+
+        $schema    = new Schema([
+            'query' => new ObjectType([
+                'name'   => 'RootQuery',
+                'fields' => [
+                    'list' => [
+                        'type'    => new ListType($unionType),
+                        'resolve' => function () {
+                            return [
+                                [
+                                    'id'   => 1,
+                                    'name' => 'name',
+                                ],
+                                [
+                                    'id'    => 2,
+                                    'title' => 'title',
+                                ],
+                            ];
+                        },
+                    ],
+                ],
+            ]),
+        ]);
+        $processor = new Processor($schema);
+        $response  = $processor->processPayload('
+{
+    list {
+        ...UnitFragment
+    }
+}
+
+fragment UnitFragment on Union {
+    __typename
+    
+    ... on Type1 {
+        id
+        name
+    }
+    ... on Type2 {
+        id
+        title
+    }
+}
+        ')->getResponseData();
+
+    }
+}

commit c4f2b6675f09f67a72d7947aeb8349352555fa66

Fixing regression introduced in https://github.com/Youshido/GraphQL/commit/8a01f948183e5d93f030e50e4016713e13cdb0e2#diff-6ba3444ce11b8aef21a5cd104bf1e5e4R357.

src/Execution/Processor.php
@@ -364,7 +364,7 @@ class Processor
                     $astFragmentModel = $astFragment->getModel();
                     $typeName         = $type->getName();
 
-                    if ($typeName !== $astFragmentModel && $type->getInterfaces()) {
+                    if ($typeName !== $astFragmentModel) {
                         foreach ($type->getInterfaces() as $interface) {
                             if ($interface->getName() === $astFragmentModel) {
                                 $result = array_replace_recursive($result, $this->collectResult($field, $type, $astFragment, $resolvedValue));